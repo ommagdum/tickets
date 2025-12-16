1f13d4a7ac318100c87e22e520a21a8c222696c6
Om Magdum <om.college.id@gmail.com>
2025-12-16 19:47:42 +0530

    feat: add search published events endpoint

--- src/main/java/com/ommagdum/tickets/services/impl/EventServiceImpl.java
+++ src/main/java/com/ommagdum/tickets/services/impl/EventServiceImpl.java
@@ -145,4 +145,9 @@ public class EventServiceImpl implements EventService {
     public Page<Event> listPublishedEvents(Pageable pageable) {
         return eventRepository.findByStatus(EventStatusEnum.PUBLISHED, pageable);
     }
+
+    @Override
+    public Page<Event> searchPublishedEvents(String query, Pageable pageable) {
+        return eventRepository.searchEvents(query, pageable);
+    }
 }
